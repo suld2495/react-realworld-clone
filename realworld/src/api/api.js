3ce4a1349962c81f052622bdc1bb49f02d925808
suld2495 <suld7777@gmail.com>
2020-11-27 22:59:45 +0900

    PIRL-87 폼으로 입력 된 정보를 DB에 저장

--- realworld/src/api/api.js
+++ realworld/src/api/api.js
@@ -1,19 +1,31 @@
 import PouchDomain from '../lib/PouchDB';
 
-const loginPouch = new PouchDomain('user');
+const realworldPouch = new PouchDomain('realworld');
+const user = 'user';
 
 export const login = async (email, password) => {
-    const users = await loginPouch.getDocs();
+    const users = await realworldPouch.getDocs(user);
     return users;
 };
 
 export const checkDuplicateUsername = async username => {
     try {
-        const result = await loginPouch.getDocs();
+        const result = await realworldPouch.getDocs(user);
         return !!result.users.find(user => {
             return user.username === username
         });
     } catch (e) {
         return false;
     }
-}
+};
+
+export const addUser = async (userData) => {
+    let data = await realworldPouch.getDocs(user) || { _id: user, users: [] };
+    data.users = [ ...data.users, userData ];
+
+    try {
+        await realworldPouch.addData(data);
+    } catch (e) {
+        console.log(e);
+    }
+};
